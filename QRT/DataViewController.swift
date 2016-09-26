d3854183dc68d8e5861cbd55a0e02f433d27c361
lucasmcdonald3 <lucasmcdonald3@gmail.com>
2016-09-26 14:01:17 -0500

    Tab menu for motor input

--- QRT/DataViewController.swift
+++ QRT/DataViewController.swift
@@ -33,7 +33,26 @@ fileprivate func >= <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
 
 class DataViewController: UIViewController {
     
+    // different text fields for inputs
+    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var text2: UITextField!
+    @IBOutlet weak var text3: UITextField!
+    @IBOutlet weak var text4: UITextField!
+    @IBOutlet weak var text5: UITextField!
+    @IBOutlet weak var text6: UITextField!
     
+    @IBOutlet weak var name1: UILabel!
+    @IBOutlet weak var text1units: UILabel!
+    @IBOutlet weak var text2units: UILabel!
+    @IBOutlet weak var text3units: UILabel!
+    
+    @IBOutlet weak var name2: UILabel!
+    @IBOutlet weak var text4units: UILabel!
+    @IBOutlet weak var text5units: UILabel!
+    @IBOutlet weak var text6units: UILabel!
+    
+    //picker view delegation
+    @IBOutlet weak var objectPicker: UIPickerView!
     
     // fields for SSH session
     var session = SSHConnection.init()
@@ -52,6 +71,8 @@ class DataViewController: UIViewController {
 
         print(session.checkConnection())
         print(session.checkAuthorization())
+        
+        raDecTabButton()
 
         
         // keyboard dismisser
@@ -138,41 +159,86 @@ class DataViewController: UIViewController {
         */
     }
     
+    func getSelectedUnits() -> Int {
+        return objectSegmentedController.selectedSegmentIndex
+    }
+    
     // checks to make sure the ra/dec values are satisfactory
     func checkMotorBoxes() -> Bool {
-        let RaHrNum = Double(RaHr.text!)
-        let RaMinNum = Double(RaMin.text!)
-        let RaSecNum = Double(RaSec.text!)
-        let DecHrNum = Double(DecHr.text!)
-        let DecMinNum = Double(DecMin.text!)
-        let DecSecNum = Double(DecSec.text!)
-        
-        if  (RaHrNum != nil) && (RaMinNum != nil) && (RaSecNum != nil) &&  (DecHrNum != nil) && (DecMinNum != nil) && (DecSecNum != nil) && (RaHrNum >= 0 && RaHrNum < 24) && (RaMinNum >= 0 && RaMinNum < 60) && (RaSecNum >= 0 && RaSecNum < 60) && (DecHrNum >= 0 && DecHrNum < 24) && (DecMinNum >= 0 && DecMinNum < 60) && (DecSecNum >= 0 && DecSecNum < 60){
-            return true
-        } else {
-            return false
+        if getSelectedUnits() == 0 { // radec
+            let text1num = Double(text1.text!)
+            let text2num = Double(text2.text!)
+            let text3num = Double(text3.text!)
+            let text4num = Double(text4.text!)
+            let text5num = Double(text5.text!)
+            let text6num = Double(text6.text!)
+            
+            if  (text1num != nil) && (text2num != nil) && (text3num != nil) &&  (text4num != nil) && (text5num != nil) && (text6num != nil) && (text1num >= 0 && text1num < 24) && (text2num >= 0 && text2num < 60) && (text3num >= 0 && text3num < 60) && (text4num >= 0 && text4num < 24) && (text5num >= 0 && text5num < 60) && (text6num >= 0 && text6num < 60){
+                return true
+            } else {
+                return false
+            }
+        } else if getSelectedUnits() == 1 { // altaz
+            let text1num = Double(text1.text!)
+            let text4num = Double(text4.text!)
+            if  (text1num != nil) &&  (text4num != nil) {
+                return true
+            } else {
+                return false
+            }
+        } else if getSelectedUnits() == 2 { // inches
+            let text1num = Double(text1.text!)
+            let text4num = Double(text4.text!)
+            if  (text1num != nil) &&  (text4num != nil) {
+                return true
+            } else {
+                return false
+            }
+        } else if getSelectedUnits() == 3 { // counts
+            let text1num = Double(text1.text!)
+            let text4num = Double(text4.text!)
+            if  (text1num != nil) &&  (text4num != nil) {
+                return true
+            } else {
+                return false
+            }
         }
+        return true
     }
     
     //converts hr/min/sec to doubles
     func getDecimalRaDec() -> [Double]{
-        let ra = Double(RaHr.text!)! + Double(RaMin.text!)!/60.0 + Double(RaSec.text!)!/3600.0
-        let dec = Double(DecHr.text!)! + Double(DecMin.text!)!/60.0 + Double(DecSec.text!)!/3600.0
+        let ra = Double(text1.text!)! + Double(text2.text!)!/60.0 + Double(text3.text!)!/3600.0
+        let dec = Double(text4.text!)! + Double(text5.text!)!/60.0 + Double(text6.text!)!/3600.0
         return [ra, dec]
     }
     
     // sends command to Pi/Pyro to initiate scanning
     func motorScan() {
-        
-        
         if !checkMotorBoxes() {
-            let alert = UIAlertController(title: "Enter a Number", message: "Please enter proper numbers in integer or decimal form in each field. Hours should be between 0 and 24; minutes and seconds should be between 0 and 60.", preferredStyle: UIAlertControllerStyle.alert)
+            let alert = UIAlertController(title: "Enter a Number", message: "Please enter proper numbers in integer or decimal form in each field.", preferredStyle: UIAlertControllerStyle.alert)
             alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
             self.present(alert, animated: true, completion: nil)
-        } else {
+        } else if getSelectedUnits() == 0 {
             let ra = getDecimalRaDec()[0]
             let dec = getDecimalRaDec()[1]
-            session.sendCommand("cd QRT/software; python SSHtoPyroController.py " + String(ra) + " " + String(dec) + " radecScan")
+            session.sendCommand("cd QRT/software; python SSHtoPyroController.py " + String(ra) + " " + String(dec) + " raDecScan")
+        } else if getSelectedUnits() == 1 {
+            let alt = Double(text1.text!)!
+            let az = Double(text4.text!)!
+            session.sendCommand("cd QRT/software; python SSHtoPyroController.py " + String(alt) + " " + String(az) + " altAzPoint")
+        } else if getSelectedUnits() == 2 {
+            let in1 = Double(text1.text!)!
+            let in2 = Double(text4.text!)!
+            session.sendCommand("cd QRT/software; python SSHtoPyroController.py " + String(in1) + " " + String(in2) + " inchesPoint")
+        } else if getSelectedUnits() == 3 {
+            let ct1 = Double(text1.text!)!
+            let ct2 = Double(text4.text!)!
+            session.sendCommand("cd QRT/software; python SSHtoPyroController.py " + String(ct1) + " " + String(ct2) + " countsPoint")
+        } else if getSelectedUnits() == 4 {
+            let selectedObject = objectData[objectPicker.selectedRow(inComponent: 0)]
+            
+            session.sendCommand("cd QRT/software; python SSHtoPyroController.py " + selectedObject + " 0 objectScan")
         }
     }
     
@@ -181,60 +247,47 @@ class DataViewController: UIViewController {
         session.sendCommand("cd QRT/software; python SSHtoPyroController.py 0 0 reset")
     }
     
-    // different text fields for RA/Dec inputs
-    @IBOutlet weak var RaHr: UITextField!
-    @IBOutlet weak var RaMin: UITextField!
-    @IBOutlet weak var RaSec: UITextField!
-    @IBOutlet weak var DecHr: UITextField!
-    @IBOutlet weak var DecMin: UITextField!
-    @IBOutlet weak var DecSec: UITextField!
     
-    //handling of next buttons in each text field
-    @IBAction func RaHrToRaMin(_ sender: UITextField) {
-        RaHr.resignFirstResponder()
-        RaMin.becomeFirstResponder()
-    }
-    
-    @IBAction func RaMinToRaSec(_ sender: UITextField) {
-        RaMin.resignFirstResponder()
-        RaSec.becomeFirstResponder()
-    }
-    
-    @IBAction func RaSecToDecHr(_ sender: UITextField) {
-        RaSec.resignFirstResponder()
-        DecHr.becomeFirstResponder()
-    }
-    
-    @IBAction func DecHrToDecMin(_ sender: UITextField) {
-        DecHr.resignFirstResponder()
-        DecMin.becomeFirstResponder()
-    }
-    
-    @IBAction func DecMinToDecSec(_ sender: UITextField) {
-        DecMin.resignFirstResponder()
-        DecSec.becomeFirstResponder()
-    }
-    
-    
-    @IBAction func DecSecDisappear(_ sender: UITextField) {
-        DecSec.resignFirstResponder()
+    @IBAction func sendMotor(_ sender: UIButton) {
         motorScan()
     }
+    
+    @IBAction func resetButton(_ sender: UIButton) {
+        motorReset()
+    }
+    
+    
+    //handling of next buttons in each text field
+    
+    @IBAction func text1end(_ sender: UITextField) {
+        text1.resignFirstResponder()
+    }
+    
+    @IBAction func text2end(_ sender: UITextField) {
+        text2.resignFirstResponder()
+    }
+    
+    @IBAction func text3end(_ sender: UITextField) {
+        text3.resignFirstResponder()
+    }
+    
+    @IBAction func text4end(_ sender: UITextField) {
+        text4.resignFirstResponder()
+    }
+
+    @IBAction func text5end(_ sender: UITextField) {
+        text5.resignFirstResponder()
+    }
+    
+    @IBAction func text6end(_ sender: UITextField) {
+        text6.resignFirstResponder()
+    }
+    
+    
     
     // buttons below text fields to send commands to Pyro
     
     
-    @IBAction func motorControlSend(_ sender: UIButton) {
-        sendMotorLength()
-    }
-    
-    @IBAction func positionResetPressed(_ sender: UIButton) {
-        motorReset()
-    }
-    
-    @IBAction func scanPressed(_ sender: UIButton) {
-        motorScan()
-    }
     
     
     
@@ -245,15 +298,8 @@ class DataViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
     
-    // send radec to scan
-    @IBAction func scanButton(_ sender: UIButton) {
-        motorScan()
-        sleep(5)
-        session.resetConnection()
-    }
     
-    //picker view delegation
-    @IBOutlet weak var objectPicker: UIPickerView!
+
     
     //returns number of columns in picker view (1)
     func numberOfComponentsInPickerView(_ pickerView: UIPickerView) -> Int {
@@ -276,6 +322,158 @@ class DataViewController: UIViewController {
         
         session.sendCommand("cd QRT/software; python SSHtoPyroController.py " + selectedObject + " 0 objectScan")
     }
+    @IBOutlet weak var objectSegmentedController: UISegmentedControl!
+    
+    
+    
+    
+    func raDecTabButton() {
+        
+        objectPicker.isHidden = true
+        
+        name1.isHidden = false
+        name2.isHidden = false
+        text1.isHidden = false
+        text2.isHidden = false
+        text3.isHidden = false
+        text4.isHidden = false
+        text5.isHidden = false
+        text6.isHidden = false
+        text1units.isHidden = false
+        text2units.isHidden = false
+        text3units.isHidden = false
+        text4units.isHidden = false
+        text5units.isHidden = false
+        text6units.isHidden = false
+        
+        name1.text = "RA:"
+        name2.text = "Dec:"
+        text1units.text = "hr"
+        text2units.text = "min"
+        text3units.text = "sec"
+        text4units.text = "hr"
+        text5units.text = "min"
+        text6units.text = "sec"
+
+        
+    }
+    
+    
+    func altAzTabButton() {
+        
+        objectPicker.isHidden = true
+        
+        name1.isHidden = false
+        name2.isHidden = false
+        text1.isHidden = false
+        text2.isHidden = true
+        text3.isHidden = true
+        text4.isHidden = false
+        text5.isHidden = true
+        text6.isHidden = true
+        text1units.isHidden = false
+        text2units.isHidden = true
+        text3units.isHidden = true
+        text4units.isHidden = false
+        text5units.isHidden = true
+        text6units.isHidden = true
+        
+        name1.text = "Alt:"
+        name2.text = "Az:"
+        text1units.text = "\u{00B0}"
+        text4units.text = "\u{00B0}"
+
+    }
+    
+    func inchesTabButton() {
+        
+        objectPicker.isHidden = true
+        
+        name1.isHidden = false
+        name2.isHidden = false
+        text1.isHidden = false
+        text2.isHidden = true
+        text3.isHidden = true
+        text4.isHidden = false
+        text5.isHidden = true
+        text6.isHidden = true
+        text1units.isHidden = false
+        text2units.isHidden = true
+        text3units.isHidden = true
+        text4units.isHidden = false
+        text5units.isHidden = true
+        text6units.isHidden = true
+        
+        name1.text = "Motor 1:"
+        name2.text = "Motor 2:"
+        text1units.text = "in"
+        text4units.text = "in"
+        
+    }
+    
+    func countsTabButton() {
+        
+        objectPicker.isHidden = true
+        
+        name1.isHidden = false
+        name2.isHidden = false
+        text1.isHidden = false
+        text2.isHidden = true
+        text3.isHidden = true
+        text4.isHidden = false
+        text5.isHidden = true
+        text6.isHidden = true
+        text1units.isHidden = false
+        text2units.isHidden = true
+        text3units.isHidden = true
+        text4units.isHidden = false
+        text5units.isHidden = true
+        text6units.isHidden = true
+        
+        name1.text = "Motor 1:"
+        name2.text = "Motor 2:"
+        text1units.text = "counts"
+        text4units.text = "counts"
+        
+    }
+    
+    func bodyTabButton() {
+        
+        objectPicker.isHidden = false
+        
+        name1.isHidden = true
+        name2.isHidden = true
+        text1.isHidden = true
+        text2.isHidden = true
+        text3.isHidden = true
+        text4.isHidden = true
+        text5.isHidden = true
+        text6.isHidden = true
+        text1units.isHidden = true
+        text2units.isHidden = true
+        text3units.isHidden = true
+        text4units.isHidden = true
+        text5units.isHidden = true
+        text6units.isHidden = true
+
+    }
+    
+    @IBAction func valueSelected(_ sender: UISegmentedControl) {
+        if objectSegmentedController.selectedSegmentIndex == 0 {
+            raDecTabButton()
+        } else if objectSegmentedController.selectedSegmentIndex == 1 {
+            altAzTabButton()
+        } else if objectSegmentedController.selectedSegmentIndex == 2 {
+            inchesTabButton()
+        } else if objectSegmentedController.selectedSegmentIndex == 3 {
+            countsTabButton()
+        } else if objectSegmentedController.selectedSegmentIndex == 4 {
+            bodyTabButton()
+        }
+    }
+    
+    
+    
     
     // button to the right of the object picker
     @IBAction func trackObjectButton(_ sender: UIButton) {
